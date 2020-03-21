7d5bd5c2a9f7391aa9cccc21134679ae727b352a
Leonardo Vinicius <leonardoviniciuswd@gmail.com>
2020-03-21 13:53:50 -0300

    Created all the files

---
+++ script.js
@@ -0,0 +1,33 @@
+function calcular() {
+	var nome = document.getElementById('userName');
+	var peso = document.getElementById('peso');
+	var altura = document.getElementById('altura');
+	var imcAtual = document.getElementById('imcAtual');
+	var imcIdeal = document.getElementById('imcIdeal');
+	var pesoIdeal = document.getElementById('pesoIdeal');
+	var perder = document.getElementById('perder');
+	var res = document.getElementById('resultado');
+	document.getElementById('resultScreen').style.display = 'block';
+
+	Number(peso.value);
+	Number(altura.value);
+	var imcAt = peso.value / ((altura.value / 100) * (altura.value / 100));
+
+	imcAtual.innerHTML = `Seu IMC atual é de ${imcAt.toFixed(2)}`;
+		if (imcAt < 18.5) {
+			res.innerHTML = 'Você está abaixo do peso';
+		} else if (imcAt < 24.9) {
+				res.innerHTML = 'Você está com o peso normal :D';
+			}
+			else if (imcAt < 29.9) {
+				res.innerHTML = 'Você está com sobrepeso';
+			}
+			else if (imcAt < 34.9) {
+				res.innerHTML = 'Você está com obesidade grau 1';
+			}
+			else if (imcAt < 39.9) {
+				res.innerHTML = 'Você está com obesidade grau 2';
+			}
+	imcIdeal.innerHTML = `O IMC Ideal é entre 18,5 e 24,9`;
+
+}
