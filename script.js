2ea5dd4ee330967fd505ddbbd0fab70de81b8c76
leonardo vinicius <leonardoviniciuswd@gmail.com>
2021-05-22 15:36:50 -0300

    Alterado condicao para evitar valores nulos

--- script.js
+++ script.js
@@ -13,28 +13,32 @@ function calcular() {
 	var imcAt = peso.value / ((altura.value / 100) * (altura.value / 100));
 
 	imcAtual.innerHTML = `Seu IMC atual é de ${imcAt.toFixed(2)}`;
-	if (nome.value.length == 0 || peso.value.length == 0 || altura.value.length ==0) {
+	/* if (!nome.value || !peso.value || !altura.value) {
 		alert('Por favor, preencha todos os dados acima');
-	} else {	
-		document.getElementById('resultScreen').style.display = 'block';
-		var sau = document.getElementById('saudacoes');
-		sau.innerHTML = `Hola, ${nome.value}!`;	
-		imcAtual.innerHTML = `Seu IMC atual é de ${imcAt.toFixed(2)}`;
-		if (imcAt < 18.5) {
-			res.innerHTML = 'Você está abaixo do peso';
-		} else if (imcAt < 24.9) {
-				res.innerHTML = 'Você está com o peso normal :D';
-			}
-			else if (imcAt < 29.9) {
-				res.innerHTML = 'Você está com sobrepeso';
-			}
-			else if (imcAt < 34.9) {
-				res.innerHTML = 'Você está com obesidade grau 1';
-			}
-			else if (imcAt < 39.9) {
-				res.innerHTML = 'Você está com obesidade grau 2';
-			}
-	imcIdeal.innerHTML = `O IMC Ideal é entre 18,5 e 24,9`;
-	}
+	} else { */
+		if (!imcAt || imcAt <= 0) {  
+			alert('Verifique os dados e preencha corretamente');
+		}	else {
+				document.getElementById('resultScreen').style.display = 'block';
+				var sau = document.getElementById('saudacoes');
+				sau.innerHTML = `Hola, ${nome.value}!`;	
+				imcAtual.innerHTML = `Seu IMC atual é de ${imcAt.toFixed(2)}`;
+				if (imcAt < 18.5) {
+					res.innerHTML = 'Você está abaixo do peso';
+				} else if (imcAt < 24.9) {
+						res.innerHTML = 'Você está com o peso normal :D';
+					}
+					else if (imcAt < 29.9) {
+						res.innerHTML = 'Você está com sobrepeso';
+					}
+					else if (imcAt < 34.9) {
+						res.innerHTML = 'Você está com obesidade grau 1';
+					}
+					else if (imcAt < 39.9) {
+						res.innerHTML = 'Você está com obesidade grau 2';
+					}
+			imcIdeal.innerHTML = `O IMC Ideal é entre 18,5 e 24,9`;
+				}
+			/*}*/
 
 }
