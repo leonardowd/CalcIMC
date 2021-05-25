9acc97c428d5882d572c40930506ad408a6e7fd1
leonardo vinicius <leonardoviniciuswd@gmail.com>
2021-05-24 23:06:15 -0300

    debugging

--- script.js
+++ script.js
@@ -11,34 +11,28 @@ function calcular() {
 	Number(peso.value);
 	Number(altura.value);
 	var imcAt = peso.value / ((altura.value / 100) * (altura.value / 100));
+	alert(imcAt);
 
 	imcAtual.innerHTML = `Seu IMC atual é de ${imcAt.toFixed(2)}`;
-	/* if (!nome.value || !peso.value || !altura.value) {
-		alert('Por favor, preencha todos os dados acima');
-	} else { */
-		if (!imcAt || imcAt <= 0) {  
-			alert('Verifique os dados e preencha corretamente');
-		}	else {
-				document.getElementById('resultScreen').style.display = 'block';
-				var sau = document.getElementById('saudacoes');
-				sau.innerHTML = `Hola, ${nome.value}!`;	
-				imcAtual.innerHTML = `Seu IMC atual é de ${imcAt.toFixed(2)}`;
-				if (imcAt < 18.5) {
-					res.innerHTML = 'Você está abaixo do peso';
-				} else if (imcAt < 24.9) {
-						res.innerHTML = 'Você está com o peso normal :D';
-					}
-					else if (imcAt < 29.9) {
-						res.innerHTML = 'Você está com sobrepeso';
-					}
-					else if (imcAt < 34.9) {
-						res.innerHTML = 'Você está com obesidade grau 1';
-					}
-					else if (imcAt < 39.9) {
-						res.innerHTML = 'Você está com obesidade grau 2';
-					}
-			imcIdeal.innerHTML = `O IMC Ideal é entre 18,5 e 24,9`;
-				}
-			/*}*/
 
-}
+		document.getElementById('resultScreen').style.display = 'block';
+		var sau = document.getElementById('saudacoes');
+		sau.innerHTML = `Hola, ${nome.value}!`;	
+		imcAtual.innerHTML = `Seu IMC atual é de ${imcAt.toFixed(2)}`;
+		if (imcAt < 18.5) {
+			res.innerHTML = 'Você está abaixo do peso';
+		} else if (imcAt < 24.9) {
+				res.innerHTML = 'Você está com o peso normal :D';
+			}
+			else if (imcAt < 29.9) {
+				res.innerHTML = 'Você está com sobrepeso';
+			}
+			else if (imcAt < 34.9) {
+				res.innerHTML = 'Você está com obesidade grau 1';
+			}
+			else if (imcAt < 39.9) {
+				res.innerHTML = 'Você está com obesidade grau 2';
+			}
+		imcIdeal.innerHTML = `O IMC Ideal é entre 18,5 e 24,9`;
+		}
+//}
